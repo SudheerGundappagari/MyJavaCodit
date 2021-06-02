beff618bec6f0d1296792c1c45a386247cb08546
sgundappagari <sgundappagari@AIPL4630LT11336.altimetrik.com>
2021-06-02 13:01:39 +0530

    Java8 Streams

---
+++ MyJavaCodit/src/com/java8/codesamples/TestMapGrouping.java
@@ -0,0 +1,61 @@
+package com.java8.codesamples;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class TestMapGrouping{
+
+    public static void main(String[] args) {
+
+        //3 apple, 2 banana, others 1
+        List<Item> items = Arrays.asList(
+                new Item("apple", 10, new BigDecimal("9.99")),
+                new Item("banana", 20, new BigDecimal("19.99")),
+                new Item("orang", 10, new BigDecimal("29.99")),
+                new Item("watermelon", 10, new BigDecimal("29.99")),
+                new Item("papaya", 20, new BigDecimal("9.99")),
+                new Item("apple", 10, new BigDecimal("9.99")),
+                new Item("banana", 10, new BigDecimal("19.99")),
+                new Item("apple", 20, new BigDecimal("9.99"))
+        );
+        
+
+        Map<String, Long> counting = items.stream().collect(
+                Collectors.groupingBy(Item::getName, Collectors.counting()));
+
+        System.out.println(counting);
+
+        Map<String, Integer> sum = items.stream().collect(
+                Collectors.groupingBy(Item::getName, Collectors.summingInt(Item::getQty)));
+
+        System.out.println(sum);
+        
+      //group by price
+        Map<BigDecimal, List<Item>> groupByPriceMap = 
+            items.stream().collect(Collectors.groupingBy(Item::getPrice));
+
+        System.out.println(groupByPriceMap);
+
+        // group by price, uses 'mapping' to convert List<Item> to Set<String>
+        Map<BigDecimal, Set<String>> result =
+                items.stream().collect(
+                        Collectors.groupingBy(Item::getPrice,
+                                Collectors.mapping(Item::getName, Collectors.toSet())
+                        )
+                );
+
+        System.out.println(result);
+        
+        
+        //group by price and agan group by quantity
+        Map<BigDecimal, Map<Integer, List<Item>>> groupByPriceMap1 = 
+            items.stream().collect(Collectors.groupingBy(Item::getPrice , Collectors.groupingBy(Item::getQty)));
+        
+        System.out.println(items.stream().filter(e -> (e.getPrice() == null)).count());
+
+    }
+}
